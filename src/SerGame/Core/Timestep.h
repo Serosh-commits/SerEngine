f0f0ffe2e4a9a263c7665eb358d395ad1716b7a2
Serosh <janmejayapanda400@gmail.com>
2026-02-04 03:28:18 +0530

    Core: Add Timestep class for delta-time

---
+++ src/SerGame/Core/Timestep.h
@@ -0,0 +1,21 @@
+#ifndef TIMESTEP_H
+#define TIMESTEP_H
+
+namespace SerGame {
+
+    class Timestep {
+    public:
+        Timestep(float time = 0.0f) : m_Time(time) {}
+
+        operator float() const { return m_Time; }
+
+        float GetSeconds() const { return m_Time; }
+        float GetMilliseconds() const { return m_Time * 1000.0f; }
+
+    private:
+        float m_Time;
+    };
+
+}
+
+#endif
