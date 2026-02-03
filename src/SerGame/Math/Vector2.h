eaa19bc194d7fccc652c22c12531597da370771c
Serosh <janmejayapanda400@gmail.com>
2026-02-04 03:28:32 +0530

    Math: Add Vector2 struct

---
+++ src/SerGame/Math/Vector2.h
@@ -0,0 +1,18 @@
+#ifndef VECTOR2_H
+#define VECTOR2_H
+
+namespace SerGame {
+
+    struct Vector2 {
+        float x, y;
+
+        Vector2() : x(0), y(0) {}
+        Vector2(float x, float y) : x(x), y(y) {}
+
+        Vector2 operator+(const Vector2& other) const { return Vector2(x + other.x, y + other.y); }
+        Vector2 operator-(const Vector2& other) const { return Vector2(x - other.x, y - other.y); }
+    };
+
+}
+
+#endif
