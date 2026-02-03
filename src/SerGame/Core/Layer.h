725353ee2f62490b5e3e19509f5339f5b593c8a4
Serosh <janmejayapanda400@gmail.com>
2026-02-04 03:28:18 +0530

    Core: Add Layer base class

---
+++ src/SerGame/Core/Layer.h
@@ -0,0 +1,27 @@
+#ifndef LAYER_H
+#define LAYER_H
+
+#include "SerGame/Events/Event.h"
+#include "SerGame/Core/Timestep.h"
+#include <string>
+
+namespace SerGame {
+
+    class Layer {
+    public:
+        Layer(const std::string& name = "Layer");
+        virtual ~Layer();
+
+        virtual void OnAttach() {}
+        virtual void OnDetach() {}
+        virtual void OnUpdate(Timestep ts) {}
+        virtual void OnEvent(Event& event) {}
+
+        inline const std::string& GetName() const { return m_DebugName; }
+    protected:
+        std::string m_DebugName;
+    };
+
+}
+
+#endif
