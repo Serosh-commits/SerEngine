e9a0400a180953380ab91ca5fbfb5065246172d0
Serosh <janmejayapanda400@gmail.com>
2026-03-11 05:18:10 +0530

    add camera header

---
+++ VoidEngine/src/Void/Renderer/Camera.h
@@ -0,0 +1,23 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glm/glm.hpp>
+
+namespace Void {
+
+    class Camera {
+    public:
+        Camera() = default;
+        Camera(const glm::mat4& projection)
+            : m_Projection(projection) {}
+
+        virtual ~Camera() = default;
+
+        const glm::mat4& GetProjection() const { return m_Projection; }
+    protected:
+        glm::mat4 m_Projection = glm::mat4(1.0f);
+    };
+
+}
+
+#endif
