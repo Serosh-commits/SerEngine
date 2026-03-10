1fcf62febdbb9e84eb5a693e465f131bcc824ce2
Serosh <janmejayapanda400@gmail.com>
2026-03-11 05:18:10 +0530

    add framebuffe source

---
+++ VoidEngine/src/Void/Renderer/Framebuffer.cpp
@@ -0,0 +1,39 @@
+#include "Framebuffer.h"
+#include <epoxy/gl.h>
+
+namespace Void {
+
+    Framebuffer::Framebuffer(uint32_t width, uint32_t height) {
+        glGenFramebuffers(1, &m_RendererID);
+        glBindFramebuffer(GL_FRAMEBUFFER, m_RendererID);
+
+        glGenTextures(1, &m_ColorAttachment);
+        glBindTexture(GL_TEXTURE_2D, m_ColorAttachment);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_ColorAttachment, 0);
+
+        glGenRenderbuffers(1, &m_DepthAttachment);
+        glBindRenderbuffer(GL_RENDERBUFFER, m_DepthAttachment);
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_DepthAttachment);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    }
+
+    Framebuffer::~Framebuffer() {
+        glDeleteFramebuffers(1, &m_RendererID);
+        glDeleteTextures(1, &m_ColorAttachment);
+        glDeleteRenderbuffers(1, &m_DepthAttachment);
+    }
+
+    void Framebuffer::Bind() {
+        glBindFramebuffer(GL_FRAMEBUFFER, m_RendererID);
+    }
+
+    void Framebuffer::Unbind() {
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    }
+
+}
