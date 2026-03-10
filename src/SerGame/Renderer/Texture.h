9c79be427b69070f2f751521021264810a8268c5
Serosh <janmejayapanda400@gmail.com>
2026-03-11 05:13:45 +0530

    add texture header

---
+++ src/SerGame/Renderer/Texture.h
@@ -0,0 +1,28 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <string>
+#include <cstdint>
+
+namespace SerGame {
+
+    class Texture {
+    public:
+        virtual ~Texture() = default;
+
+        virtual uint32_t GetWidth() const = 0;
+        virtual uint32_t GetHeight() const = 0;
+
+        virtual void Bind(uint32_t slot = 0) const = 0;
+
+        static std::shared_ptr<Texture> Create(const std::string& path);
+    };
+
+    class Texture2D : public Texture {
+    public:
+        static std::shared_ptr<Texture2D> Create(const std::string& path);
+    };
+
+}
+
+#endif
